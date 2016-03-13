4f60fb152d15152311461a9f925cac1974194d2a
Wil Andrade <wilandrade@users.noreply.github.com>
2016-03-13 03:15:30 -0700

    Remove number array that wasn't being used.

--- wait/App/Components/WeightSetup.js
+++ wait/App/Components/WeightSetup.js
@@ -68,7 +68,6 @@ class WeightSetup extends Component {
 	}
 
 	render() {
-		const possibleNums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 		return (
 			<View style={styles.container}>
 				<View style={styles.top}>
