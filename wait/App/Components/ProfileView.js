42d22f24e0a1fd985610332304989a3f021e9ae8
George Lam <georgelam0306@gmail.com>
2016-03-13 01:48:16 -0800

    add profile fix tableview

--- wait/App/Components/ProfileView.js
+++ wait/App/Components/ProfileView.js
@@ -1,16 +1,38 @@
 'use strict';
 
 import React, {
-	Component,
-	View,
-	Text,
-	Navigator
+  Component,
+  View,
+  Text,
+  ScrollView,
+  StyleSheet
 } from 'react-native';
+import {
+  Cell,
+  CustomCell,
+  Section,
+  TableView
+} from 'react-native-tableview-simple';
 
 export default class ProfileView extends Component {
-	render() {
-		return (<View>
-				<Text>Profile View</Text>
-			</View>)
-	}
+  render() {
+    return (
+      <ScrollView style={{marginTop: 60}} contentContainerStyle={styles.stage}>
+        <TableView>
+          <Section header="Profile">
+            <Cell cellstyle="RightDetail" title="Username" detail="test"/>
+            <Cell cellstyle="RightDetail" title="Goal Weight" detail="Goal Weight" onPress={() => console.log('Heyho!')}/>
+            <Cell cellstyle="RightDetail" title="Weight Loss Per Week" detail="0.5" onPress={() => console.log('Heyho!')}/>
+          </Section>
+        </TableView>
+      </ScrollView>)
+  }
 }
+
+const styles = StyleSheet.create({
+  stage: {
+    backgroundColor: '#EFEFF4',
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+});
