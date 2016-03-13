d9c946cb88323ed35a438b34f9524fc222b9d2ca
George Lam <georgelam0306@gmail.com>
2016-03-12 22:15:10 -0800

    add tab view

--- wait/App/Components/App.js
+++ wait/App/Components/App.js
@@ -3,13 +3,37 @@
 import React, {
 	Component,
 	View,
-	Text
+	Text,
+	Navigator
 } from 'react-native';
+
+import ReactNativeRouter, {Route, Schema, Animations, TabBar} from 'react-native-router-flux';
+import {connect} from 'react-redux';
+import ChartView from './ChartView';
+
+const Router = connect()(ReactNativeRouter.Router);
+
+class TabIcon extends React.Component {
+    render(){
+        return (
+            <Text style={{color: this.props.selected ? 'red' :'black'}}>{this.props.title}</Text>
+        );
+    }
+}
 
 export default class App extends Component {
 	render() {
-		return <View>
-			<Text>Entry point </Text>
-		</View>
+		return (
+			<Router hideNavBar={true}>
+			<Schema name="modal" sceneConfig={Navigator.SceneConfigs.FloatFromBottom}/>
+      <Schema name="default" sceneConfig={Navigator.SceneConfigs.FloatFromRight}/>
+      <Schema name="withoutAnimation"/>
+			<Schema name="tab" type="switch" icon={TabIcon} />
+			<Route name="tabbar">
+          <Router footer={TabBar} hideNavBar={true} tabBarStyle={{borderTopColor:'#00bb00',borderTopWidth:1,backgroundColor:'white'}}>
+              <Route name="chartView" schema="tab" title="Chart" component={ChartView} />
+          </Router>
+      </Route>
+			</Router>)
 	}
 }
---
+++ wait/App/Components/ChartView.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React, {
+	Component,
+	View,
+	Text
+} from 'react-native';
+
+export default class ChartView extends Component {
+	render() {
+		return <View>
+			<Text>This is the chart</Text>
+		</View>
+	}
+}
