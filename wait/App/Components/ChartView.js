a6a37e1790e39a2b6075c90d89217eb8538e8afb
George Lam <georgelam0306@gmail.com>
2016-03-12 22:19:54 -0800

    add react native chart

--- wait/App/Components/ChartView.js
+++ wait/App/Components/ChartView.js
@@ -1,15 +1,53 @@
-'use strict';
+import React, { StyleSheet, View, Component } from 'react-native';
+import RNChart from 'react-native-chart';
 
-import React, {
-	Component,
-	View,
-	Text
-} from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    chart: {
+        position: 'absolute',
+        top: 65,
+        left: 4,
+        bottom: 55,
+        right: 16,
+    }
+});
+
+const chartData = [
+    {
+        name: 'BarChart',
+        type: 'bar',
+        color:'purple',
+        widthPercent: 0.6,
+        data: [30, 1, 1, 2, 3, 5, 21, 13, 21, 34, 55, 30],
+    },
+    {
+        name: 'LineChart',
+        color: 'gray',
+        lineWidth: 2,
+        highlightIndices: [1, 2],   // The data points at indexes 1 and 2 will be orange
+        highlightColor: 'orange',
+        showDataPoint: true,
+        data: [10, 12, 14, 25, 31, 52, 41, 31, 52, 66, 22, 11],
+    }
+];
+
+const xLabels = ['0','1','2','3','4','5','6','7','8','9','10','11'];
 
 export default class ChartView extends Component {
-	render() {
-		return <View>
-			<Text>This is the chart</Text>
-		</View>
-	}
+    render() {
+        return (
+            <View style={styles.container}>
+                <RNChart style={styles.chart}
+                    chartData={chartData}
+                    verticalGridStep={5}
+                    xLabels={xLabels}
+                 />
+            </View>
+        );
+    }
 }
