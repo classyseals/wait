94fbeedbd4e94d9d971f8bcf594c92952a1c1ea6
George Lam <georgelam0306@gmail.com>
2016-03-12 23:57:45 -0800

    add art project

--- wait/App/Components/ChartView.js
+++ wait/App/Components/ChartView.js
@@ -18,42 +18,46 @@ const styles = StyleSheet.create({
 });
 
 
-const xLabels = ['0','1','2','3','4','5','6','7','8','9','10','11'];
+const xLabels = ['0','1','2','3','4','5'];
 
 export default class ChartView extends Component {
     constructor(props) {
         super(props);
-        let progressData = props.weightRecords.map((weightRecord)=> {
-          return weightRecord.get('weight');
-        });
+        this.componentWillReceiveProps(props);
+    }
+    componentWillReceiveProps(props) {
+    	let progressData = props.weightRecords.map((weightRecord)=> {
+    	  return weightRecord.get('weight');
+    	});
 
-        let chartData = [
-            {
-                name: 'Progress',
-                color: 'gray',
-                lineWidth: 2,
-                highlightColor: 'orange',
-                showDataPoint: true,
-                data: progressData.toArray().reverse(),
-            },
-            {
-                name: 'Goal',
-                color: 'red',
-                lineWidth: 2,
-                showDataPoint: true,
-                data: [props.weightRecords.get(props.weightRecords.size - 1).get('weight'), props.goalWeight],
-            }
-        ];
-        this.state = {
-          chartData
-        }
+    	let chartData = [
+    	    {
+    	        name: 'Progress',
+    	        color: 'gray',
+    	        lineWidth: 2,
+    	        highlightIndices: [1,2,3,4],
+    	        highlightColor: 'orange',
+    	        showDataPoint: true,
+    	        data: progressData.toArray().reverse(),
+    	    },
+    	    {
+    	        name: 'Goal',
+    	        color: 'red',
+    	        lineWidth: 2,
+    	        data: [props.weightRecords.get(props.weightRecords.size - 1).get('weight'), 0, 0, props.goalWeight],
+    	    }
+    	];
+    	this.state = ({
+    	  chartData
+    	});
     }
     render() {
         return (
             <View style={styles.container}>
                 <RNChart style={styles.chart}
                     chartData={this.state.chartData}
-                    verticalGridStep={5}
+                    verticalGridStep={10}
+                    tightBounds={true}
                     xLabels={xLabels}
                  />
             </View>
