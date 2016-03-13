eee392e8894be9a0270f14396c06caa6f81f3285
George Lam <georgelam0306@gmail.com>
2016-03-12 21:31:21 -0800

    use app.js

---
+++ wait/App/Components/App.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React, {
+	Component,
+	View,
+	Text
+} from 'react-native';
+
+export default class App extends Component {
+	render() {
+		return <View>
+			<Text>Entry point </Text>
+		</View>
+	}
+}
